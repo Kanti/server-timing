2709b6ea6b48186081df68087d8fa90fbc4f5d5b
Matthias Vogel <m.vogel@andersundsehr.com>
2022-08-06 17:58:53 +0200

    ♻️ refecter ExtbaseDispatcher into separate files

---
+++ Classes/XClass/ExtbaseDispatcherV11.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Kanti\ServerTiming\XClass;
+
+use Kanti\ServerTiming\Utility\TimingUtility;
+use Psr\Http\Message\ResponseInterface;
+use TYPO3\CMS\Extbase\Mvc\Dispatcher;
+use TYPO3\CMS\Extbase\Mvc\Request;
+use TYPO3\CMS\Extbase\Mvc\Web\Request as WebRequest;
+use TYPO3\CMS\Extbase\Mvc\RequestInterface;
+
+class ExtbaseDispatcherV11 extends Dispatcher
+{
+    public function dispatch(RequestInterface $request): ResponseInterface
+    {
+        $str = 'extbase ' . str_replace('\\', '_', $request->getControllerObjectName());
+        if ($request instanceof Request) {
+            $str .= '->' . $request->getControllerActionName();
+        }
+        $stop = TimingUtility::stopWatch($str);
+        $response = parent::dispatch($request);
+        $stop();
+        return $response;
+    }
+}
--- ext_localconf.php
+++ ext_localconf.php
@@ -2,15 +2,22 @@
 
 use Kanti\ServerTiming\Middleware\AdminpanelSqlLoggingMiddleware;
 use Kanti\ServerTiming\Utility\GuzzleUtility;
-use Kanti\ServerTiming\XClass\ExtbaseDispatcher;
+use Kanti\ServerTiming\XClass\ExtbaseDispatcherLegacy;
 use Kanti\ServerTiming\XClass\CoreRequestFactory;
+use Kanti\ServerTiming\XClass\ExtbaseDispatcherV11;
 use TYPO3\CMS\Adminpanel\Middleware\SqlLogging;
 use TYPO3\CMS\Core\Http\RequestFactory;
 use TYPO3\CMS\Extbase\Mvc\Dispatcher;
 
-$GLOBALS['TYPO3_CONF_VARS']['SYS']['Objects'][Dispatcher::class] = [
-    'className' => ExtbaseDispatcher::class,
-];
+if (version_compare(TYPO3_branch, '11.0', '>=')) {
+    $GLOBALS['TYPO3_CONF_VARS']['SYS']['Objects'][Dispatcher::class] = [
+        'className' => ExtbaseDispatcherV11::class,
+    ];
+} else {
+    $GLOBALS['TYPO3_CONF_VARS']['SYS']['Objects'][Dispatcher::class] = [
+        'className' => ExtbaseDispatcherLegacy::class,
+    ];
+}
 $GLOBALS['TYPO3_CONF_VARS']['SYS']['Objects'][SqlLogging::class] = [
     'className' => AdminpanelSqlLoggingMiddleware::class,
 ];
