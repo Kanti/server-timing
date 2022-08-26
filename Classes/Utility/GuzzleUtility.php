ca7aa4031e57982ec7cd1f801059e3a29ef5b340
Matthias Vogel <git@kanti.de>
2022-08-26 13:26:31 +0200

    🐛 fix rejected Guzzle RejectedPromise error

--- Classes/Utility/GuzzleUtility.php
+++ Classes/Utility/GuzzleUtility.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 namespace Kanti\ServerTiming\Utility;
 
 use Closure;
-use GuzzleHttp\Promise\FulfilledPromise;
+use GuzzleHttp\Promise\PromiseInterface;
 use Psr\Http\Message\RequestInterface;
 
 class GuzzleUtility
@@ -13,7 +13,7 @@ class GuzzleUtility
     public static function getHandler(): Closure
     {
         return static function (callable $handler): Closure {
-            return static function (RequestInterface $request, array $options) use ($handler): FulfilledPromise {
+            return static function (RequestInterface $request, array $options) use ($handler): PromiseInterface {
                 $info = $request->getMethod() . ' ' . $request->getUri()->__toString();
                 $stop = TimingUtility::stopWatch('guzzle', $info);
                 $response = $handler($request, $options);
