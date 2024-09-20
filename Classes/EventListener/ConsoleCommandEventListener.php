b889769be6f1d983b09a3a48fb3ae02c640a8e6e
Itay <102981181+N0S0I@users.noreply.github.com>
2024-09-20 16:10:43 +0200

    fix usage with crontab
    When a command is executing another command, there is no longer an error

--- Classes/EventListener/ConsoleCommandEventListener.php
+++ Classes/EventListener/ConsoleCommandEventListener.php
@@ -5,21 +5,27 @@ declare(strict_types=1);
 namespace Kanti\ServerTiming\EventListener;
 
 use Kanti\ServerTiming\Dto\ScriptResult;
-use Kanti\ServerTiming\SqlLogging\SqlLoggerCore11;
 use Kanti\ServerTiming\Utility\TimingUtility;
 use Symfony\Component\Console\Event\ConsoleCommandEvent;
 use Symfony\Component\Console\Event\ConsoleTerminateEvent;
+use Kanti\ServerTiming\Dto\StopWatch;
 
 final class ConsoleCommandEventListener
 {
+    /** @var StopWatch[] */
+    private array $stopWatches = [];
+
     public function start(ConsoleCommandEvent $event): void
     {
-        TimingUtility::start('console.command', (string)$event->getCommand()?->getName());
+        $this->stopWatches[] = TimingUtility::stopWatch('console.command', (string)$event->getCommand()?->getName());
     }
 
     public function stop(ConsoleTerminateEvent $event): void
     {
-        TimingUtility::end('console.command');
-        TimingUtility::getInstance()->shutdown(ScriptResult::fromCli($event->getExitCode()));
+        $stopWatch = array_pop($this->stopWatches);
+        $stopWatch->stop();
+        if (!$this->stopWatches) {
+            TimingUtility::getInstance()->shutdown(ScriptResult::fromCli($event->getExitCode()));
+        }
     }
 }
